7f8ad82619e7d186f9bbe35e87bfa3a2a6aaab6b
vwency <veniaminshp@gmail.com>
2026-02-21 15:17:17 +0600

    chore: remove comments

--- app/src/components/pages/landing/index.tsx
+++ app/src/components/pages/landing/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '@/styles/MainLayout/index.scss'
-import Footer from '../../footer/Footer' // <- путь к Footer.tsx относительно MainLayout
+import Footer from '../../footer/Footer'
 
 const MainLayout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
     <div className="main__layout no-select">
-      {children} {/* Основной контент страницы */}
-      <Footer /> {/* Футер */}
+      {children} 
+      <Footer /> 
     </div>
   )
 }
